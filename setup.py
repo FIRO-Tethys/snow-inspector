1d3377cb190ce240bf51d3f098916892836c295a
Rohit Khattar <rohitkh@byu.edu>
2020-09-14 19:41:16 -0600

    Restored Setup.py and cleaned up conda recipe

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
 from setuptools import setup, find_namespace_packages
-import os
-
-
-def find_resource_files(directory, relative_to=None):
-    paths = []
-    for (path, directories, filenames) in os.walk(directory):
-        for filename in filenames:
-            if relative_to is not None:
-                paths.append(os.path.join(os.path.relpath(path, relative_to), filename))
-            else:
-                paths.append(os.path.join('..', path, filename))
-    return paths
-
+from tethys_apps.app_installation import find_resource_files
 
 # -- Apps Definition -- #
 app_package = 'snow_inspector'
@@ -20,6 +8,7 @@ release_package = 'tethysapp-' + app_package
 # -- Get Resource File -- #
 resource_files = find_resource_files('tethysapp/' + app_package + '/templates', 'tethysapp/' + app_package)
 resource_files += find_resource_files('tethysapp/' + app_package + '/public', 'tethysapp/' + app_package)
+resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces', 'tethysapp/' + app_package)
 
 # -- Python Dependencies -- #
 dependencies = []
