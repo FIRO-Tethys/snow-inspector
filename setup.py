6f142951a4b85fe2e558271db34706d9e728e6b9
Corey Krewson <ckrewson@aquaveo.com>
2023-12-27 15:43:12 +0000

    tag version v3.0.0_0_2023_12_27

--- setup.py
+++ setup.py
@@ -1,32 +1,30 @@
 from setuptools import setup, find_namespace_packages
-from setup_helper import find_resource_files
+from setup_helper import find_all_resource_files
 
 # -- Apps Definition -- #
-app_package = 'snow_inspector'
-release_package = 'tethysapp-' + app_package
-
-# -- Get Resource File -- #
-resource_files = find_resource_files('tethysapp/' + app_package + '/templates', 'tethysapp/' + app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/public', 'tethysapp/' + app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces', 'tethysapp/' + app_package)
+namespace = 'tethysapp'
+app_package = "snow_inspector"
+release_package = "tethysapp-" + app_package
 
 # -- Python Dependencies -- #
 dependencies = []
 
-resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' + app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/scripts', 'tethysapp/' + app_package)
+# -- Get Resource File -- #
+resource_files = find_all_resource_files(app_package, namespace)
+
+
 setup(
     name=release_package,
-    version='3.0.0',
-    description='Snow Inspector',
-    long_description='',
-    keywords='',
-    author='',
-    author_email='',
-    url='',
-    license='',
+    version="3.0.0",
+    description="Snow Inspector",
+    long_description="Snow Inspector",
+    keywords="replace_keywords",
+    author="",
+    author_email="_email",
+    url="",
+    license="",
     packages=find_namespace_packages(),
-    package_data={'': resource_files},
+    package_data={"": resource_files},
     include_package_data=True,
     zip_safe=False,
     install_requires=dependencies,
